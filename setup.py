6c1533a5338b1b2f4a0fb1b9ff0f47554b1b3684
Jason Garman <jgarman@bit9.com>
2015-09-22 15:32:13 -0400

    fix DEVR-109

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ scripts = {
 
 setup(
     name='python-cb-lastline-connector',
-    version='1.0',
+    version='1.1',
     packages=['cbopensource', 'cbopensource.connectors', 'cbopensource.connectors.lastline'],
     url='https://github.com/carbonblack/cb-lastline-connector',
     license='MIT',
