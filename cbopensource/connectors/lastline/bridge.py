c090dffd29de3c4efe92102edf3d5915907545c3
Jason McFarland <jmcfarland@carbonblack.com>
2017-06-12 14:06:55 -0500

    upgrade to cbint 0.8.9 and inc version

--- cbopensource/connectors/lastline/bridge.py
+++ cbopensource/connectors/lastline/bridge.py
@@ -99,7 +99,7 @@ class LastlineConnector(DetonationDaemon):
 
     @property
     def integration_name(self):
-        return 'Cb LastLine Connector 1.2.5'
+        return 'Cb LastLine Connector 1.2.6'
 
     @property
     def num_quick_scan_threads(self):
