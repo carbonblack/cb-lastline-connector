9446d51343f60f99591dfc11981c8d0d03a18b86
Zachary Estep <zestep@carbonblack.com>
2022-02-08 10:54:59 -0500

    CB-37038: Add container build

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-systemctl start cb-lastline-connector
+/usr/share/cb/integrations/lastline/bin/cb-lastline-connector start
 PID=$(cat /var/run/cb/integrations/cb-lastline-connector.pid)
 while [ -e /proc/$PID ]
 do
     echo "Process: $PID is still running" >> /dev/null
+    sleep 1
 done
